b6c742fb59e095f94a2069fa5eb4caec5da0f566
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-26 08:27:30 -0500

    changed site title

--- webpack.common.js
+++ webpack.common.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Webpack app',
+      title: "Goose's Goulash",
       filename: 'index.html',
       template: 'src/template.html',
     }),
