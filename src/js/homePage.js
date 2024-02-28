4fc83836d90c8a03180b911de5967a95c6f5f636
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-27 21:28:36 -0500

    created empty functions to create panels and append to #content

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,11 +1,37 @@
+// SET HOW MANY PANELS FOR HOME PAGE
 const HOMEPANELS = 4;
+
+const content = document.querySelector('#content');
+
+// CREATE PANEL ELEMENTS ARRAY
+let panels = [];
+for (let i = 0; i < HOMEPANELS; i++) {
+  const homePagePanel = document.createElement('div');
+  homePagePanel.classList.add(`panel-${i}`);
+  panels.push(homePagePanel);
+}
+
+function videoPanel() {
+
+}
+
+function facebookPanel() {
+
+}
+
+function contactPanel() {
+
+}
+
+function mapPanel() {
+
+}
+
 function loadHomePage() {
-  const content = document.querySelector('#content');
-  for (let i = 0; i < 8; i++) {
-    const homePagePanel = document.createElement('div');
-    homePagePanel.classList.add(`panel-${i}`);
-    content.appendChild(homePagePanel);
-  }
+  videoPanel();
+  facebookPanel();
+  contactPanel();
+  mapPanel();
 }
 
 export { loadHomePage };
