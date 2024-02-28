bbff1af3507947b107e80b8a1815c595edf58e64
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-27 21:40:03 -0500

    finished panel above header function

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,37 +1,48 @@
 // SET HOW MANY PANELS FOR HOME PAGE
-const HOMEPANELS = 4;
+const HOMEPANELS = 5;
 
 const content = document.querySelector('#content');
+const homeBody = document.querySelector('#home');
 
 // CREATE PANEL ELEMENTS ARRAY
 let panels = [];
 for (let i = 0; i < HOMEPANELS; i++) {
   const homePagePanel = document.createElement('div');
+  homePagePanel.classList.add('panel');
   homePagePanel.classList.add(`panel-${i}`);
   panels.push(homePagePanel);
 }
 
-function videoPanel() {
+function memberPanel(panel) {
+  const innerPanel = document.createElement('h3');
+  innerPanel.textContent = "Now Accepting New Members";
 
+  panel.appendChild(innerPanel);
+  homeBody.prepend(panel);
 }
 
-function facebookPanel() {
-
+function videoPanel(panel) {
+  content.appendChild(panel);
 }
 
-function contactPanel() {
-
+function facebookPanel(panel) {
+  content.appendChild(panel);
 }
 
-function mapPanel() {
+function contactPanel(panel) {
+  content.appendChild(panel);
+}
 
+function mapPanel(panel) {
+  content.appendChild(panel);
 }
 
 function loadHomePage() {
-  videoPanel();
-  facebookPanel();
-  contactPanel();
-  mapPanel();
+  memberPanel(panels[0]);
+  videoPanel(panels[1]);
+  facebookPanel(panels[2]);
+  contactPanel(panels[3]);
+  mapPanel(panels[4]);
 }
 
 export { loadHomePage };
