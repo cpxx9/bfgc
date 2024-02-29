e4b8523a04f60874046014cb0a3fc41905fa73ac
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-02-29 08:04:49 -0500

    add event listeners to all buttons

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,9 @@ import '../styles/style.css';
 import { loadHomePage } from "./homePage";
 
 loadHomePage();
+
+let navButtons = document.querySelectorAll('.nav-btn');
+console.log(navButtons);
+navButtons.forEach((navButton) => {
+  navButton.addEventListener('click', navButton.dataset.function);
+})
