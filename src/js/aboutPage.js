c5c4ec65af661fe49824bc52a56fbfe8d21c6786
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-01 12:27:29 -0500

    added title

--- src/js/aboutPage.js
+++ src/js/aboutPage.js
@@ -2,7 +2,14 @@ import { loadConnectWithUs } from "./connectWithUs";
 
 function loadAboutPage() {
   loadConnectWithUs();
-  console.log('about test');
+  const container = document.querySelector('#content');
+
+  const aboutTitle = document.createElement('h1');
+  aboutTitle.textContent = "About Our Club";
+  const aboutTitleSplit = document.createElement('hr');
+
+  container.appendChild(aboutTitle);
+  container.appendChild(aboutTitleSplit);
 }
 
 export { loadAboutPage };
