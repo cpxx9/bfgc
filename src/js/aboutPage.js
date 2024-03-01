d975bea4e87ab98daf53e10ab9c66c630fc1e4ed
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-01 16:50:14 -0500

    base card dom structure set

--- src/js/aboutPage.js
+++ src/js/aboutPage.js
@@ -4,16 +4,32 @@ function loadAboutPage() {
   loadConnectWithUs();
   const container = document.querySelector('#content');
 
-  const aboutTitle = document.createElement('h1');
-  aboutTitle.textContent = "About Our Club";
-  const aboutTitleSplit = document.createElement('hr');
+  const titleContainer = document.createElement('div');
+  titleContainer.classList.add('title-container');
+    const aboutTitle = document.createElement('h1');
+    aboutTitle.textContent = "About Our Club";
+    const aboutTitleSplit = document.createElement('hr');
+  titleContainer.appendChild(aboutTitle);
+  titleContainer.appendChild(aboutTitleSplit);
 
-  const mainCard = document.createElement('div');
-  mainCard.classList.add('card', 'main-card');
+  const cardsContainer = document.createElement('div');
+  cardsContainer.classList.add('cards-container');
+    const mainCard = document.createElement('div');
+    mainCard.classList.add('card', 'main-card');
+    const cards = document.createElement('div');
+    cards.classList.add('cards');
+  cardsContainer.appendChild(mainCard);
+  cardsContainer.appendChild(cards);
 
-  container.appendChild(aboutTitle);
-  container.appendChild(aboutTitleSplit);
-  container.appendChild(mainCard);
+  const NUMBER_OF_CARDS = 2;
+  for(let i = 0; i < NUMBER_OF_CARDS; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card', `card-${i}`);
+    cards.appendChild(card);
+  }
+
+  container.appendChild(titleContainer);
+  container.appendChild(cardsContainer);
 }
 
 export { loadAboutPage };
