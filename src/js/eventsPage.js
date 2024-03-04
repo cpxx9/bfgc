fee4d65e9f0bfeca138c6cd22ce775cf63c30cd1
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-03 20:49:18 -0500

    added title to page

--- src/js/eventsPage.js
+++ src/js/eventsPage.js
@@ -2,7 +2,19 @@ import { loadConnectWithUs } from "./connectWithUs";
 
 function loadEventsPage() {
   loadConnectWithUs();
-  console.log('event test');
+  const container = document.querySelector("#content");
+  container.classList.add('event-page');
+
+  const titleContainer = document.createElement('div');
+  titleContainer.classList.add('title-container');
+    const eventTitle = document.createElement('h1');
+    eventTitle.textContent = "UPCOMING EVENTS";
+    eventTitle.classList.add('page-heading');
+    const eventTitleSplit = document.createElement('hr');
+  titleContainer.appendChild(eventTitle);
+  titleContainer.appendChild(eventTitleSplit);
+  
+  container.appendChild(titleContainer);
 }
 
 export { loadEventsPage };
