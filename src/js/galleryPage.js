45ad7bcab2b5129896dfe79044980fbcfed6da00
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-03-04 08:05:28 -0500

    base DOM structure

--- src/js/galleryPage.js
+++ src/js/galleryPage.js
@@ -5,6 +5,18 @@ function loadGalleryPage() {
   
   const content = document.querySelector('#content');
   content.classList.add('gallery-page');
+
+  const galleryEventButton = document.createElement('button');
+  const gridQuoteWrapper = document.createElement('div'); 
+  gridQuoteWrapper.classList.add('gq-wrapper');
+  const galleryGridArea = document.createElement('div');
+  galleryGridArea.classList.add('gallery-grid-area');
+  const galleryQuote = document.createElement('div');
+  galleryQuote.classList.add('gallery-quote');
+  gridQuoteWrapper.appendChild(galleryGridArea);
+  gridQuoteWrapper.appendChild(galleryQuote);
+  content.appendChild(galleryEventButton);
+  content.appendChild(gridQuoteWrapper);
 }
 
 export { loadGalleryPage };
